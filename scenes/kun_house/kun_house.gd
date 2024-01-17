c7900336fc0492192f799f37fbe9dc79ba401d43
dxwb <1131432970@qq.com>
2024-01-17 14:03:09 +0800

    场景中最多10只小鸡

--- scenes/kun_house/kun_house.gd
+++ scenes/kun_house/kun_house.gd
@@ -5,6 +5,8 @@ extends StaticBody2D
 @onready var sprite_2d = $Sprite2D
 @onready var spawn_timer = $SpawnTimer
 
+var kun_count = 3
+
 func _ready():
 	set_random_wait_time()
 	spawn_timer.start()
@@ -20,16 +22,21 @@ func _on_area_2d_area_entered(area: Area2D):
 	var tween = get_tree().create_tween()
 	tween.tween_property(sprite_2d, 'scale', Vector2(1.0, 1.5), .1)
 	tween.tween_property(sprite_2d, 'scale', Vector2(1.0, 1.0), .5).set_trans(Tween.TRANS_BACK)
+	kun_count -= 1
 
 # 让小鸡从鸡舍里面逃出来
 func _on_spawn_timer_timeout():
-	var x = randi_range(-100, 100)
+	# 场景中最多10只小鸡
+	if (kun_count >= 10): return
+	
+	var x = randi_range(-200, 200)
 	var y = randi_range(50, 200)
 	var pos = global_position + Vector2(x, y)
 	var kun: Kun = kun_packed_scene.instantiate()
 	kun.global_position = global_position
 	kun.leave_kun_house(pos)
 	get_tree().get_current_scene().add_child(kun)
+	kun_count += 1
 
 	# 生成小鸡之后，设置随机时间
 	set_random_wait_time()
