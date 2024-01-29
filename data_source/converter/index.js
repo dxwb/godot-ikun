0a6e3a42ff9299f0a2c8338034bd30e62076ddcc
dxwb <1131432970@qq.com>
2024-01-29 14:54:18 +0800

    excel转换器完成

--- data_source/converter/index.js
+++ data_source/converter/index.js
@@ -1,3 +1,8 @@
+const fs = require('fs')
+const XLSX = require('xlsx')
+
+const url = './data_table.xlsx'
+
 const keypress = () => {
   process.stdin.setRawMode(true)
   return new Promise(resolve => process.stdin.once('data', () => {
@@ -6,9 +11,29 @@ const keypress = () => {
   }))
 }
 
+const importFileString = `[remap]
+
+importer="keep"
+`
+
 const run = async () => {
-  console.log(1)
-  console.log('🌟转换完成，按任意键继续')
+  const workbook = XLSX.readFile(url)
+  workbook.SheetNames.forEach(name => {
+    if (!name.includes('|')) return
+
+    let csvString = XLSX.utils.sheet_to_csv(workbook.Sheets[name])
+
+    csvString = csvString.replaceAll('"ID"', 'ID')
+
+    const nameArr = name.split('|')
+    const enName = nameArr[0]
+    const fileName = `${enName}.csv`
+
+    fs.writeFileSync(`./csv/${fileName}`, csvString)
+    fs.writeFileSync(`./csv/${fileName}.import`, importFileString)
+    console.log(`【${name}】工作表转换完成`)
+  })
+  console.log('🌟所有工作表都已经转换完成，按任意键继续')
   await keypress()
   process.exit()
 }
