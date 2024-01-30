185b6be56d660fea6c673190fc78831624f20e9a
dxwb <1131432970@qq.com>
2024-01-30 22:42:01 +0800

    将流程管理器抽离

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,4 +1,7 @@
 extends Node
 
+func _ready():
+	ProcedureManager.begin_game.connect(_on_procedure_manager_begin_game)
+
 func _on_procedure_manager_begin_game():
 	SceneManager.change_scene(Strings.GAME_SCENE)
