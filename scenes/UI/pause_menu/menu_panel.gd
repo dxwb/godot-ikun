c36d7cb40287d19a6328206b0a312e988a1a4182
dxwb <1131432970@qq.com>
2024-02-03 16:28:29 +0800

    增加暂停菜单

---
+++ scenes/UI/pause_menu/menu_panel.gd
@@ -0,0 +1,4 @@
+extends PanelContainer
+class_name MenuPanel
+
+@export var icon: Texture2D
--- scenes/UI/pause_menu/pause_menu.gd
+++ scenes/UI/pause_menu/pause_menu.gd
@@ -2,11 +2,28 @@ extends Control
 
 @export var icon: Texture2D
 
-@onready var tab_container = $TabContainer
+@onready var tab_container = %TabContainer
 
 func _ready():
-	tab_container.set_tab_icon(0, icon)
-	tab_container.set_tab_title(0, "")
+	var _children = tab_container.get_children()
+	for i in _children.size():
+		tab_container.set_tab_title(i, "")
+		tab_container.set_tab_icon(i, _children[i].icon)
 
-func _process(delta):
-	pass
+func _unhandled_key_input(event):
+	if event.is_action("ui_cancel") && event.is_pressed():
+		if visible:
+			close()
+		else:
+			open()
+
+func open():
+	show()
+	get_tree().paused = true
+
+func close():
+	hide()
+	get_tree().paused = false
+
+func _on_close_button_pressed():
+	close()
