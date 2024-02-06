c8f4e84af476d296a4ea4914be7f9344498284aa
dxwb <1131432970@qq.com>
2024-02-06 11:31:38 +0800

    挑战展示和交互修改

--- UI/pause_menu/challenge_panel/challenge_item.gd
+++ UI/pause_menu/challenge_panel/challenge_item.gd
@@ -3,7 +3,8 @@ extends PanelContainer
 @export var check = false
 
 @onready var texture_rect = %TextureRect
-@onready var label = %Label
+@onready var title = %Title
+@onready var condition = %Condition
 
 const ITEM_CHECK_STYLE = preload("res://UI/pause_menu/challenge_panel/item_check_style.tres")
 const ITEM_UNCHECK_STYLE = preload("res://UI/pause_menu/challenge_panel/item_uncheck_style.tres")
@@ -13,20 +14,6 @@ signal click()
 func _ready():
 	set_check(check)
 
-func _on_mouse_entered():
-	if not check: return
-
-	z_index = 1
-	var tween := create_tween()
-	tween.tween_property(self, "scale", Vector2(1.1, 1.1), .1)
-
-func _on_mouse_exited():
-	if not check: return
-
-	z_index = 0
-	var tween := create_tween()
-	tween.tween_property(self, "scale", Vector2(1, 1), .1)
-
 func _on_gui_input(event):
 	if not check: return
 
@@ -34,12 +21,10 @@ func _on_gui_input(event):
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			click.emit()
 
-func _on_resized():
-	pivot_offset = size / 2
-
 func set_challenge(chal_data: Dictionary):
 	texture_rect.texture = load(chal_data.texture)
-	label.text = chal_data.title
+	title.text = chal_data.title
+	condition.text = chal_data.condition
 
 func set_check(value: bool):
 	check = value
@@ -47,8 +32,8 @@ func set_check(value: bool):
 	if value:
 		set("theme_override_styles/panel", ITEM_CHECK_STYLE)
 		modulate = Color.WHITE
-		mouse_default_cursor_shape = CursorShape.CURSOR_POINTING_HAND
+		condition.set("theme_override_colors/font_color", Color(.6, .6, .6))
 	else:
 		set("theme_override_styles/panel", ITEM_UNCHECK_STYLE)
 		modulate = Color(.6, .6, .6)
-		mouse_default_cursor_shape = CursorShape.CURSOR_ARROW
+		condition.set("theme_override_colors/font_color", Color.WHITE)
