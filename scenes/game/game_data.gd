0e3e7044fb3f23ce4fb37304947a9e03bc2fe2ae
dxwb <1131432970@qq.com>
2024-02-22 15:15:33 +0800

    判断商人和老虎机的显示隐藏

--- scenes/game/game_data.gd
+++ scenes/game/game_data.gd
@@ -5,6 +5,8 @@ extends Node
 @onready var caught_chickens_ui = %CaughtChickens
 @onready var ghost_spawn_count_ui = %GhostSpawnCount
 @onready var snowman_hit_count_ui = %SnowmanHitCount
+@onready var store = %Store
+@onready var slot_machine = %SlotMachine
 
 func _ready():
 	_init_ui()
@@ -20,6 +22,8 @@ func _on_player_died():
 
 	SaverLoader.save_game()
 
+	_check_store_visible()
+
 func _on_kun_house_kun_entered(area):
 	var caught_kuns = SaverLoader.running_data.caught_kuns + 1
 
@@ -30,6 +34,8 @@ func _on_kun_house_kun_entered(area):
 	ChallengeService.complete_challenge_by_group("KUNS", caught_kuns)
 
 	SaverLoader.save_game()
+
+	_check_store_visible()
 
 func _on_enemy_spawner_enemy_spawned(enemy):
 	var ghost_spawn_count = SaverLoader.running_data.ghost_spawn_count + 1
@@ -42,6 +48,8 @@ func _on_enemy_spawner_enemy_spawned(enemy):
 
 	SaverLoader.save_game()
 
+	_check_store_visible()
+
 func _on_snowman_player_hit():
 	var snowman_hit_count = SaverLoader.running_data.snowman_hit_count + 1
 
@@ -53,12 +61,16 @@ func _on_snowman_player_hit():
 
 	SaverLoader.save_game()
 
+	_check_store_visible()
+
 func _init_ui():
 	glod_count_ui.set_num(SaverLoader.running_data.glod)
 	caught_chickens_ui.set_num(SaverLoader.running_data.caught_kuns)
 	death_count_ui.set_num(SaverLoader.running_data.death_count)
 	ghost_spawn_count_ui.set_num(SaverLoader.running_data.ghost_spawn_count)
 	snowman_hit_count_ui.set_num(SaverLoader.running_data.snowman_hit_count)
+	_check_store_visible()
+	_check_slot_machine_visible()
 
 func _on_store_traded(data: Dictionary):
 	var glod = SaverLoader.running_data.glod + data.amount
@@ -81,8 +93,22 @@ func _on_store_traded(data: Dictionary):
 
 	SaverLoader.save_game()
 
+	_check_slot_machine_visible()
+
 func _on_slot_machine_init_slot_machine_played(cost):
 	SaverLoader.running_data.glod -= cost
 	glod_count_ui.set_num(SaverLoader.running_data.glod)
 
 	SaverLoader.save_game()
+
+func _check_store_visible():
+	if (SaverLoader.running_data.caught_kuns > 0 and
+		SaverLoader.running_data.death_count > 0 and
+		SaverLoader.running_data.ghost_spawn_count > 0 and
+		SaverLoader.running_data.snowman_hit_count > 0):
+		store.visible = true
+	else:
+		store.visible = false
+
+func _check_slot_machine_visible():
+	slot_machine.visible = SaverLoader.running_data.glod > 0
