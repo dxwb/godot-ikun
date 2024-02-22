9bb14fa334c4f1f81a7027739e266588d5c30ce8
dxwb <1131432970@qq.com>
2024-02-22 14:32:52 +0800

    增加暂停菜单设置界面

--- UI/pause_menu/setting_panel/setting_panel.gd
+++ UI/pause_menu/setting_panel/setting_panel.gd
@@ -1,11 +1,13 @@
 extends MenuPanel
 
+signal scene_before_leave()
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_button_pressed():
+	scene_before_leave.emit()
+	SceneManager.change_scene(Strings.TITLE_MENU_SCENE)
