9be28944b65059997b36fb634a6b762db2f822d9
dxwb <1131432970@qq.com>
2024-03-07 15:29:59 +0800

    增加掉落动画

--- common/loot/loot.gd
+++ common/loot/loot.gd
@@ -2,8 +2,13 @@ extends Node2D
 
 @export var move_speed = 200
 
+@onready var pick_up_receiver = $PickUpReceiver
+
 var target: Area2D
 var loot_data: Dictionary
+
+func _ready():
+	throw()
 
 func _process(delta):
 	_move_to_target(delta)
@@ -21,3 +26,13 @@ func _move_to_target(delta):
 
 	var direction = (target.global_position - global_position).normalized()
 	global_position += direction * move_speed * delta
+
+func throw():
+	var tween = create_tween()
+
+	tween.tween_property(self, "position:y", position.y - 10, .1).set_ease(Tween.EASE_OUT)
+	tween.tween_property(self, "position:y", position.y, .1).set_ease(Tween.EASE_IN)
+
+	await tween.finished
+	
+	pick_up_receiver.visible = true
