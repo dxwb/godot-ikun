465a6af24bb285acdc3ea91df0b8972379460990
dxwb <1131432970@qq.com>
2024-03-26 22:50:10 +0800

    修复设置没保存成功的问题

--- UI/setting_ui/setting_ui.gd
+++ UI/setting_ui/setting_ui.gd
@@ -10,11 +10,8 @@ func _init_setting():
 	music_volume_setting.h_slider.value = SettingManager.data.music_volume
 	vfx_volume_setting.h_slider.value = SettingManager.data.sfx_volume
 
-func _on_volume_changed(bus_index, value):
-	match bus_index:
-		1:
-			SettingManager.data.music_volume = value
-		2:
-			SettingManager.data.sfx_volume = value
+func _on_music_volume_setting_volume_changed(value):
+	SettingManager.set_music_volume(value)
 
-	SettingManager.save_setting()
+func _on_vfx_volume_setting_volume_changed(value):
+	SettingManager.set_sfx_volume(value)
--- auto/setting_manager/setting_manager.gd
+++ auto/setting_manager/setting_manager.gd
@@ -28,5 +28,29 @@ func load_setting():
 	data.music_volume = config.get_value("Volumes", "music_volume", 50)
 	data.sfx_volume = config.get_value("Volumes", "sfx_volume", 50)
 
+	set_music_volume(data.music_volume)
+	set_sfx_volume(data.sfx_volume)
+
 func _on_debounce_timer_timeout():
 	config.save(file_path)
+
+func set_sfx_volume(value: int):
+	data.sfx_volume = value
+	_set_volume(2, value)
+
+func set_music_volume(value: int):
+	data.music_volume = value
+	_set_volume(1, value)
+
+func _set_volume(bus_index: int, value: float):
+	if value == 0:
+		AudioServer.set_bus_mute(bus_index, true)
+	else:
+		AudioServer.set_bus_mute(bus_index, false)
+		var db = _convert_percentage(value)
+		AudioServer.set_bus_volume_db(bus_index, db)
+
+func _convert_percentage(percentage: float) -> float:
+	var c_scale = 20.0
+	var divisor = 50.0
+	return c_scale * (log(percentage / divisor) / log(10))
