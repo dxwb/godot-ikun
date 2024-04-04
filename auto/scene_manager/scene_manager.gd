182ecd320f27b33a55bc9341aef61536dce1a82e
dxwb <1131432970@qq.com>
2024-04-04 08:53:32 +0800

    优化加载场景效果

--- auto/scene_manager/scene_manager.gd
+++ auto/scene_manager/scene_manager.gd
@@ -1,6 +1,36 @@
 extends CanvasLayer
 
+@onready var animation_player = $AnimationPlayer
+
 var current_scene_name: StringName
+
+func transition_in_scene(scene_name: StringName):
+	play_transition_in()
+
+	await animation_player.animation_finished
+
+	change_scene(scene_name)
+
+func transition_in_scene_to_packed(packed_scene: PackedScene):
+	play_transition_in()
+
+	await animation_player.animation_finished
+
+	change_scene_to_packed(packed_scene)
+
+func transition_out_scene(scene_name: StringName):
+	play_transition_out()
+
+	await animation_player.animation_finished
+
+	change_scene(scene_name)
+
+func transition_out_scene_to_packed(packed_scene: PackedScene):
+	play_transition_out()
+
+	await animation_player.animation_finished
+
+	change_scene_to_packed(packed_scene)
 
 func change_scene(scene_name: StringName):
 	get_tree().change_scene_to_file(scene_name)
@@ -10,7 +40,18 @@ func change_scene_to_packed(packed_scene: PackedScene):
 
 func change_scene_async(scene_name: StringName):
 	current_scene_name = scene_name
-	change_scene(Strings.LOADING_SCENE)
+	transition_out_scene(Strings.LOADING_SCENE)
+
+func play_transition_in():
+	show()
+	animation_player.play("transition")
+
+func play_transition_out():
+	show()
+	animation_player.play_backwards("transition")
 
 #func _add_a_scene_manually(scene: Node):
 	#get_tree().root.add_child(scene)
+
+func _on_animation_player_animation_finished(anim_name):
+	hide()
--- scenes/loading/loading.gd
+++ scenes/loading/loading.gd
@@ -4,6 +4,7 @@ extends Node2D
 
 var scene_path: StringName
 var progress: Array
+var update := 0.0
 
 func _ready():
 	scene_path = SceneManager.current_scene_name
@@ -11,8 +12,14 @@ func _ready():
 
 func _process(delta):
 	ResourceLoader.load_threaded_get_status(scene_path, progress)
-	progress_bar.value = progress[0] * 100
 
 	if progress_bar.value >= 100:
 		var packed_scene = ResourceLoader.load_threaded_get(scene_path)
 		SceneManager.change_scene_to_packed(packed_scene)
+		SceneManager.play_transition_in()
+
+	if progress[0] > update:
+		update = progress[0]
+
+	if progress_bar.value < update * 100:
+		progress_bar.value += delta * 100
